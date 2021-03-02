4376218aeb191192505432d4646c5eb61eced464
Diego Souza <diegobueno.souza@gmail.com>
2021-03-01 22:02:34 -0300

    Metodos Get GetId Post Put e Delete Departamentos

--- src/main/java/br/com/opefit/projeto/recursos/DepartamentoRecurso.java
+++ src/main/java/br/com/opefit/projeto/recursos/DepartamentoRecurso.java
@@ -1,12 +1,19 @@
 package br.com.opefit.projeto.recursos;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.opefit.projeto.entidades.Departamento;
 import br.com.opefit.projeto.servicos.DepartamentoServico;
@@ -22,6 +29,31 @@ public class DepartamentoRecurso {
 	public ResponseEntity<List<Departamento>> findAll(){						
 		List<Departamento> list = departamentoServico.findAll();
 		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Departamento> findById(@PathVariable Long id) {
+		Departamento obj = departamentoServico.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+
+	@PostMapping
+	public ResponseEntity<Departamento> insert(@RequestBody Departamento obj) {
+		obj = departamentoServico.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
+	}
+
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id) {
+		departamentoServico.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+
+	@PutMapping(value="/{id}")
+	public ResponseEntity<Departamento> update(@PathVariable Long id, @RequestBody Departamento obj) {
+		obj = departamentoServico.updateDepartamento(id, obj);
+		return ResponseEntity.ok().body(obj);
 	}
 
 
--- src/main/java/br/com/opefit/projeto/servicos/DepartamentoServico.java
+++ src/main/java/br/com/opefit/projeto/servicos/DepartamentoServico.java
@@ -1,6 +1,7 @@
 package br.com.opefit.projeto.servicos;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,12 +11,34 @@ import br.com.opefit.projeto.repositorios.DepartamentoRepositorio;
 
 @Service
 public class DepartamentoServico {
-	
+
 	@Autowired
 	private DepartamentoRepositorio departamentoRepositorio;
-	
-	public List<Departamento> findAll(){
+
+	public List<Departamento> findAll() {
 		return departamentoRepositorio.findAll();
+	}
+
+	public Departamento findById(Long id) {
+		Optional<Departamento> obj = departamentoRepositorio.findById(id);
+		return obj.get();
+	}
+
+	public Departamento insert(Departamento usuario) {
+		return departamentoRepositorio.save(usuario);
+	}
+
+	public void delete(Long id) {
+		Departamento obj = findById(id);
+		departamentoRepositorio.delete(obj);
+	}
+
+	public Departamento updateDepartamento(Long id, Departamento departamento) {
+		Departamento user = departamentoRepositorio.getOne(id);
+
+		user.setNome(departamento.getNome());
+
+		return departamentoRepositorio.save(user);
 	}
 
 }
